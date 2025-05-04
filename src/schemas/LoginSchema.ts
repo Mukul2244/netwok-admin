450ae47805135a64c5460b590c3cc144b29a8594
Mukul2244 <mukulsharma1594@gmail.com>
2025-05-04 20:11:04 +0530

    fix: update login schema validation messages and strengthen password requirements;

--- src/schemas/LoginSchema.ts
+++ src/schemas/LoginSchema.ts
@@ -3,10 +3,14 @@ import { z } from "zod";
 export const LoginSchema = z.object({
   username: z
     .string()
-    .min(3, { message: "Username must have at least 3 characters." })
-    .max(32, { message: "Username cannot exceed 32 characters." }),
+    .min(3, { message: "Username is required" })
+    .max(32, { message: "Username is too long" }),
+
   password: z
     .string()
-    .min(6, { message: "Password must have at least 6 characters." })
-    .max(32, { message: "Password cannot exceed 32 characters." }),
+    .min(8, "Password must be at least 8 characters long")
+    .regex(/[A-Z]/, "Must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Must contain at least one number")
+    .regex(/[@$!%*?&#]/, "Must contain at least one special character"),
 });
